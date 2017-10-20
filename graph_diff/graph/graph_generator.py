eb4888ed9fd1a812c707cb27de1e79a153c70ece
bzik <alexander.bzikadze@gmail.com>
2017-10-20 20:01:24 +0300

    Comments deleted from graph_generator

--- graph_diff/graph/graph_generator.py
+++ graph_diff/graph/graph_generator.py
@@ -42,28 +42,4 @@ class GraphGenerator:
                     if 1 == nrandom.randint(2) and node not in [new_node, GraphWithRepetitiveNodesWithRoot._ROOT]:
                         graph.add_edge_exp(node, new_node)
 
-
-        # node_number = random.randint(self.min_node_num, self.max_node_num)
-        #
-        # generated_nodes = 1
-        # len_in_labels = []
-        # while generated_nodes < node_number:
-        #     generate_label = random.randint(1, node_number - generated_nodes + 1)
-        #     len_in_labels.append(generate_label)
-        #     for i in range(1, generate_label + 1):
-        #         graph.add_node(lr_node(len(len_in_labels), i))
-        #     generated_nodes += generate_label
-        # graph_density = random.random()
-        # added_edges = 0
-        # import math
-        # print(len_in_labels)
-        # print(generated_nodes, node_number)
-        # for _ in range(0, int(math.floor(graph_density * node_number ** 2))):
-        #     label1 = random.randint(1, len(len_in_labels) - 1)
-        #     label2 = random.randint(label1 + 1, len(len_in_labels))
-        #     number1 = random.randint(1, len_in_labels[label1 - 1] + 1)
-        #     number2 = random.randint(1, len_in_labels[label2 - 1] + 1)
-        #     graph.add_edge(lr_node(label1, number1), lr_node(label2, number2))
-        # return graph
-
         return graph
