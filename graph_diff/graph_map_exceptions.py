7365ac5fb23151271985cca0dfa4374425a2cdf5
bzik <alexander.bzikadze@gmail.com>
2018-03-10 22:52:36 +0300

    graph_map_exception docs

--- graph_diff/graph_map_exceptions.py
+++ graph_diff/graph_map_exceptions.py
@@ -1,3 +1,8 @@
 class GraphDoesNotContainMappedNodeException(Exception):
+    """
+    Exception to be thrown GraphMap.
+    Indicates that node is mapped to nonexistent node.
+    """
+
     def __init__(self, message):
         self.message = message
