db209e562892909b4b5fb1b1e04a062cb138fc15
bzik <alexander.bzikadze@gmail.com>
2018-03-10 23:31:22 +0300

    Graph map docs; graph map exceptions remade; comparators moved to another file

--- graph_diff/graph_map_exceptions.py
+++ graph_diff/graph_map_exceptions.py
@@ -4,5 +4,7 @@ class GraphDoesNotContainMappedNodeException(Exception):
     Indicates that node is mapped to nonexistent node.
     """
 
-    def __init__(self, message):
-        self.message = message
+    def __init__(self,
+                 error_list: list):
+        self.message = 'There are unmatched nodes during GraphMap.construct_graph_map:\n' \
+                       + '\n'.join('{} is not found in graph {}'.format(node, num) for node, num in error_list)
